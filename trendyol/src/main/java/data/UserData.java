54d24db40d8c9067b47ab96477e0989126346327
Berkay Barlas <yusufberkaybarlas@gmail.com>
2022-08-04 19:59:07 +0300

    delete userinfot

--- trendyol/src/main/java/data/UserData.java
+++ trendyol/src/main/java/data/UserData.java
@@ -3,7 +3,7 @@ package data;
 public class UserData {
 
     public static final String userEmail = "thmaxwyenruschtdbl@kvhrw.com";
-    public static final String userPassword = "test1234";
+    public static final String userPassword = "";
     public static final String wrongEmail = "thmaxwyenruschtdbl1234@kvhrw.com";
     public static final String emptyEmail = " ";
     public static final String wrongPassword = "test";
